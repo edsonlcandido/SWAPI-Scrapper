72c7a09b58be5309a0604bd2575e19d4685200ed
Edson Candido <Edson.Candido@howden.com>
2024-04-23 12:24:30 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text.Json;
+using System.Net.Http;
+
+namespace SWAPI_Scrapper
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Root films = await GetFilms();
+
+            Console.WriteLine("Hello, World!");
+
+        }
+
+        static async Task<Root> GetFilms()
+        {
+            using var client = new HttpClient();
+            var url = "https://swapi.py4e.com/api/films/?format=json";
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var jsonString = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<Root>(jsonString);
+        }
+    }
+
+    public class Film
+    {
+        public string title { get; set; }
+        public int episode_id { get; set; }
+        public string opening_crawl { get; set; }
+        public string director { get; set; }
+        public string producer { get; set; }
+        public string release_date { get; set; }
+        public List<string> characters { get; set; }
+        public List<string> planets { get; set; }
+        public List<string> starships { get; set; }
+        public List<string> vehicles { get; set; }
+        public List<string> species { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Root
+    {
+        public int count { get; set; }
+        public object next { get; set; }
+        public object previous { get; set; }
+        public List<Film> results { get; set; }
+    }
+    public class Character
+    {
+        public string name { get; set; }
+        public string height { get; set; }
+        public string mass { get; set; }
+        public string hair_color { get; set; }
+        public string skin_color { get; set; }
+        public string eye_color { get; set; }
+        public string birth_year { get; set; }
+        public string gender { get; set; }
+        public string homeworld { get; set; }
+        public List<Film> films { get; set; }
+        public List<string> species { get; set; }
+        public List<string> vehicles { get; set; }
+        public List<string> starships { get; set; }
+        public DateTime created { get; set; }
+        public DateTime edited { get; set; }
+        public string url { get; set; }
+    }
+
+}
